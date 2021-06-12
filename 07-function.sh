535ee7dc6781824c44191ddb3ed01c377029eec2
SAI-RRR <onlinesvr4@gmail.com>
2021-06-12 20:56:51 +0530

    function to print values of a and b

---
+++ 07-function.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+##DECLARE A FUNCTION
+
+SAMPLE()
+ {
+   echo WELCOME TO FUNCTIONS
+   echo value of a is $a
+   b=10
+ }
+ ## ACCESS A FUNCTION
+ a=10
+ SAMPLE
+ echo value of b is $b
