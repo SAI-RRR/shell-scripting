18e8c1de10ee5d60fd0a5ab53105c078cb74aa77
SAI-RRR <onlinesvr4@gmail.com>
2021-06-15 19:16:59 +0530

    common loops while and for  demo while to print values from 10 to 1 and for loop to print bike names

--- 09-loops.sh
+++ 09-loops.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
-##how while loop works
 i=10
-while[$i -gt 0] ; do
-echo $i
-i=$(($i-1))
+while [ $i -gt 0 ]; do
+  echo $i
+  i=$(($i-1))
 done
-for bike in yamaha,honda,hero;do
-  echo bike Name = $bike
+
+# for var in values ; do
+# commands
+# done
+
+for fruit in apple banana orange ; do
+  echo Fruit Name = $fruit
   sleep 1
 done
