23faeee0d8ff7a09fc6f9e5a7d1b47f11381d4c5
SAI-RRR <onlinesvr4@gmail.com>
2021-07-07 16:51:46 +0530

    printing my name in print name print hi about she-bang and scripting languages

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,9 @@ echo -e "Hello world \n welcome to universe"
 echo Sai
 
 echo Welcome to shell scripting
+echo -e "\n#! \nis called She-bang"
+echo -e "A scripting language is used to write scripts.
+ These contain a series of commands that are interpreted one by one at runtime unlike programming languages
+ that are compiled first before running.Nowadays, scripts are generally associated with web development
+ where they are widely used to make dynamic web applications. Scripting languages can be divided into
+ two categories:"
