0aaaf3648cdfd6a89e4cd001c05cd9d59819e358
SAI-RRR <onlinesvr4@gmail.com>
2021-07-10 20:48:46 +0530

    printing my name in print name print hi about she-bang and scripting languages print text in colors giving the infomation about no of users logged into system

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Declare vaiablle
+## Declare variable
 
 COURSE=DEVOPS
 #ACCES A VARIABLE
@@ -10,6 +10,9 @@ echo COURSE NAME=$COURSE
 ##DATE=09-06-2021
 DATE=$(date +%F)
 echo Good Morning,Today Date is $DATE
+
+NO_OF_USERS=$(who | wc -l)
+echo  NO of users logged into system = $NO_OF_USERS
 ADD=$((2+2*3))
 echo $ADD
 BIG=$((2+3*4-1))
