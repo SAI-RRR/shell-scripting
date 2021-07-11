8cfaf8c571f0736272471999497c9fde12aea09e
SAI-RRR <85189455+SAI-RRR@users.noreply.github.com>
2021-07-11 18:52:45 +0530

    special variables along with their descirptions

--- 05-specialvar.sh
+++ 05-specialvar.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+## to get script name
 echo $0
+## to get first argument
 echo $1
+## to get second argument
 echo $2
+## to get the all arguments passed
 echo $*
 echo $@
+## to get the number of arguments
 echo $#
