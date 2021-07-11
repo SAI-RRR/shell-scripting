f0cc038147ecab439f82e7f1afbecd3db4082bbd
SAI-RRR <85189455+SAI-RRR@users.noreply.github.com>
2021-07-11 18:45:03 +0530

    asked to enter first and last name also

--- 04-read.sh
+++ 04-read.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 read -p 'Enter ur name' name
 echo "your name =$name"
+read -p 'Enter ur first name' firstname
+echo "your first name =$firstname"
+read -p 'Enter last name' lastname
+echo "your last name=$lastname"
