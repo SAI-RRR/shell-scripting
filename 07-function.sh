454cb5aec31168c3d0d98e0a347cffcb21457778
SAI-RRR <85189455+SAI-RRR@users.noreply.github.com>
2021-07-11 21:55:50 +0530

    fucntion basics definition return and exit function

--- 07-function.sh
+++ 07-function.sh
@@ -5,10 +5,12 @@
 SAMPLE()
  {
    echo WELCOME TO FUNCTIONS
+   return 5
    echo value of a is $a
    b=10
  }
  ## ACCESS A FUNCTION
  a=20
  SAMPLE
+ echo Exit status of SAMPLE function =$?
  echo value of b is $b
